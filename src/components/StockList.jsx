a9b22d119796866007de3891a5c77cc4b85cc036
sone <sthaya@gmail.com>
2022-09-01 19:37:07 -0700

    setup search for symbol with dropdown

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,9 +1,18 @@
 import { useState, useEffect } from "react";
+import { BsFillCaretDownFill, BsFillCaretUpFill } from "react-icons/bs";
 import finnHub from "../apis/finnHub";
 
 const StockList = () => {
   const [stock, setStock] = useState([]);
   const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"]);
+
+  const changeColor = (change) => {
+    return change > 0 ? "success" : "danger";
+  };
+
+  const renderIcon = (change) => {
+    return change > 0 ? <BsFillCaretUpFill /> : <BsFillCaretDownFill />;
+  };
 
   useEffect(() => {
     let isMounted = true;
@@ -38,7 +47,44 @@ const StockList = () => {
     return () => (isMounted = false);
   }, []);
 
-  return <div>StockList</div>;
+  return (
+    <div>
+      <table className="table hover mt-5">
+        <thead style={{ color: "rgb(79,89,102)" }}>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Last</th>
+            <th scope="col">Chg</th>
+            <th scope="col">Chg%</th>
+            <th scope="col">High</th>
+            <th scope="col">Low</th>
+            <th scope="col">Open</th>
+            <th scope="col">Pclose</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stock.map((stockData) => {
+            return (
+              <tr className="table-row" key={stockData.symbol}>
+                <th>{stockData.symbol}</th>
+                <td>{stockData.data.c}</td>
+                <td className={`text-${changeColor(stockData.data.d)}`}>
+                  {stockData.data.d} {renderIcon(stockData.data.d)}
+                </td>
+                <td className={`text-${changeColor(stockData.data.dp)}`}>
+                  {stockData.data.dp} {renderIcon(stockData.data.dp)}
+                </td>
+                <td>{stockData.data.h}</td>
+                <td>{stockData.data.l}</td>
+                <td>{stockData.data.o}</td>
+                <td>{stockData.data.pc}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default StockList;
